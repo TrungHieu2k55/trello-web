21a2025532fdd61b6af8ce0f56ba51909fde6bf7
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2025-07-01 22:40:36 +0700

    Install mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Button from '@mui/material/Button'
+<<<<<<< HEAD
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
@@ -13,15 +14,41 @@ function App() {
       <Button variant='outlined'>Outlined</Button>
 
       <br />
+=======
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import HomeIcon from '@mui/icons-material/Home';
+import { pink } from '@mui/material/colors';
+
+function App() {
+
+
+  return (
+    <>
+      <div>Trung hieu</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <br/>
+>>>>>>> 4b0af36 (Install mui)
       <AccessAlarmIcon />
       <ThreeDRotation />
 
       <HomeIcon />
+<<<<<<< HEAD
       <HomeIcon color='primary' />
       <HomeIcon color='secondary' />
       <HomeIcon color='success' />
       <HomeIcon color='action' />
       <HomeIcon color='disabled' />
+=======
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+>>>>>>> 4b0af36 (Install mui)
       <HomeIcon sx={{ color: pink[500] }} />
     </>
   )
