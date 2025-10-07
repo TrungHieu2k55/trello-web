e7be4fc4e6af4d0bf88941a6c9a13863a9712e90
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2025-10-07 20:49:52 +0700

    refactor_theme_color

--- src/page/Boards/BoardContent/index.jsx
+++ src/page/Boards/BoardContent/index.jsx
@@ -1,9 +1,13 @@
+import { useColorScheme } from '@mui/material'
 import Box from '@mui/material/Box'
 
 function BoardContent() {
+  const { mode, systemMode } = useColorScheme()
+  // Nếu mode = system thì lấy systemMode, ngược lại lấy mode
+  const resolvedMode = mode === 'system' ? systemMode : mode
   return (
     <Box sx={{
-      backgroundColor: 'primary.main',
+      bgcolor: resolvedMode === 'dark' ? '#34495e' : '#1976d2',
       width: '100%',
       height: (theme) => `calc(100vh - ${theme.trello.boardBarheight} - ${theme.trello.appBarHeight})`,
       display: 'flex',
