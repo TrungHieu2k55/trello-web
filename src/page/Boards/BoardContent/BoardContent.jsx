c1325887e34c05b1c25177301412db2ef6fea65a
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2025-12-01 21:41:07 +0700

    keo tha columns

--- src/page/Boards/BoardContent/BoardContent.jsx
+++ src/page/Boards/BoardContent/BoardContent.jsx
@@ -3,21 +3,68 @@ import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sort'
 
+import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useState, useEffect } from 'react'
+
 function BoardContent({ board }) {
-  const oderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // dùng PointerSensor mặc định thì phải kết hợp thuộc tính touchAction: 'none' ở phần tử kéo thả còn bug
+  const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+
+  // yêu cầu chuột di chuyển 10px thì mới kich hoạt event, fix trường hợp click bị gọi event
+  const mouseSensor = useSensorr(MouseSensor, { activationConstraint: { distance: 10 } })
+
+  // nhấn giữ 250ms và tolerance 500px thì kích hoạt sự kiện kéo thả trên thiết bị cảm ứng
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+
+  // Ưu tiên sử dụng kết hợp 2 loại dùng mouseSensor và touchSensor để trải nghiệm trên mobile, ít bị bug hơn
+  const sensors = useSensors(pointerSensor)
+
+
+  const [oderedColumns, setOderedColumns] = useState([])
+
+  useEffect(() => {
+    setOderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    console.log('Drag Ended', event)
+    const { active, over } = event
+
+
+    // kiểm tra nếu không tồn tại over (kéo linh tinh ra ngoài thì return luôn tránh lỗi)
+    if (!over) return
+
+    if (active.id !== over.id) {
+      // Tạo lại thứ tự cột
+      const oldIndex = oderedColumns.findIndex(c => c._id === active.id)
+      const newIndex = oderedColumns.findIndex(c => c._id === over.id)
+
+      // arraymove để sắp xếp lại mảng columns ban đầu
+      // code arraymove dnt-kit/package/sortable/src/utilities/arrayMove.ts
+      const dndOrderedColumns = arrayMove(oderedColumns, oldIndex, newIndex)
+
+      // xử lý gọi  api
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log('dndOrderedColumnsIds', dndOrderedColumnsIds )
+      setOderedColumns(dndOrderedColumns)
+    }
+  }
 
   const { mode, systemMode } = useColorScheme()
   // Nếu mode = system thì lấy systemMode, ngược lại lấy mode
   const resolvedMode = mode === 'system' ? systemMode : mode
   return (
-    <Box sx={{
-      bgcolor: resolvedMode === 'dark' ? '#34495e' : '#1976d2',
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColumns columns={ oderedColumns } />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box sx={{
+        bgcolor: resolvedMode === 'dark' ? '#34495e' : '#1976d2',
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={ oderedColumns } />
+      </Box>
+    </DndContext>
   )
 }
 
--- src/page/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/page/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -19,9 +19,24 @@ import AddCardIcon from '@mui/icons-material/AddCard'
 import DragHandleIcon from '@mui/icons-material/DragHandle'
 import ListCards from './ListCards/ListCards'
 import { mapOrder } from '~/utils/sort'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 
 function Column({ column }) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: column._id,
+    data: { ...column }
+  })
+  const dndKitColumnStyles = {
+    // touchAction: 'none', // dành cho pointerSensor
+    // Nếu sử dụng CSS.Transform của doc sẽ bị strech
+    // https://github.com/clauderic/dnd-kit/issues/117
+    transform: CSS.Translate.toString(transform),
+    transition
+  }
+
+
   const [anchorEl, setAnchorEl] = React.useState(null)
   const open = Boolean(anchorEl)
   const handleClick = (event) => setAnchorEl(event.currentTarget)
@@ -32,15 +47,21 @@ function Column({ column }) {
   // Nếu mode = system thì lấy systemMode, ngược lại lấy mode
   const resolvedMode = mode === 'system' ? systemMode : mode
   return (
-    <Box sx={{
-      minWidth: '300px',
-      maxWidth: '300px',
-      bgcolor: resolvedMode === 'dark' ? '#333643' : '#ebecf0',
-      ml:2,
-      borderRadius: '6px',
-      height: 'fit-content',
-      maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
-    }}>
+    <Box
+      ref={setNodeRef}
+      style={dndKitColumnStyles}
+      {...attributes}
+      {...listeners}
+      sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: resolvedMode === 'dark' ? '#333643' : '#ebecf0',
+        ml:2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
+      }}
+    >
       { /* Column Header */ }
       <Box sx={{
         height: (theme) => theme.trello.columnHeaderHeight,
