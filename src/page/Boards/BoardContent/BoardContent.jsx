f4b712499d8f4d5ed17f543416bfecec9ef7e342
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2025-12-05 18:50:19 +0700

    drag drop overlay

--- src/page/Boards/BoardContent/BoardContent.jsx
+++ src/page/Boards/BoardContent/BoardContent.jsx
@@ -3,32 +3,52 @@ import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sort'
 
-import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
+import { DndContext, PointerSensor, useSensor, useSensors,
+  MouseSensor, TouchSensor, DragOverlay, defaultDropAnimationSideEffects } from '@dnd-kit/core'
 import { arrayMove } from '@dnd-kit/sortable'
 import { useState, useEffect } from 'react'
+import Column from './ListColumns/Column/Column'
+import Card from './ListColumns/Column/ListCards/Card/Card'
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+  COLUMN: 'ACTIVE_DRAG_ITEM_TYPE_COLUMN',
+  CARD: 'ACTIVE_DRAG_ITEM_TYPE_CARD'
+}
 
 function BoardContent({ board }) {
   // dùng PointerSensor mặc định thì phải kết hợp thuộc tính touchAction: 'none' ở phần tử kéo thả còn bug
-  const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
 
   // yêu cầu chuột di chuyển 10px thì mới kich hoạt event, fix trường hợp click bị gọi event
-  const mouseSensor = useSensorr(MouseSensor, { activationConstraint: { distance: 10 } })
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
 
   // nhấn giữ 250ms và tolerance 500px thì kích hoạt sự kiện kéo thả trên thiết bị cảm ứng
   const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
 
   // Ưu tiên sử dụng kết hợp 2 loại dùng mouseSensor và touchSensor để trải nghiệm trên mobile, ít bị bug hơn
-  const sensors = useSensors(pointerSensor)
+  const sensors = useSensors( mouseSensor, touchSensor )
 
 
   const [oderedColumns, setOderedColumns] = useState([])
+
+  //cùng một thời điểm chỉ có 1 phần tử được kéo (card hoặc column)
+  const [activeDragItemId, setActiveDragItemId] = useState(null)
+  const [activeDragItemType, setActiveDragItemType] = useState(null)
+  const [activeDragItemData, setActiveDragItemData] = useState(null)
 
   useEffect(() => {
     setOderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
   }, [board])
 
+  const handleDragStart = (event) => {
+    // console.log('Drag Started', event)
+    setActiveDragItemId(event?.active?.id)
+    setActiveDragItemType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_TYPE.CARD : ACTIVE_DRAG_ITEM_TYPE.COLUMN)
+    setActiveDragItemData(event?.active?.data?.current)
+  }
+
   const handleDragEnd = (event) => {
-    console.log('Drag Ended', event)
+    // console.log('Drag Ended', event)
     const { active, over } = event
 
 
@@ -49,13 +69,28 @@ function BoardContent({ board }) {
       // console.log('dndOrderedColumnsIds', dndOrderedColumnsIds )
       setOderedColumns(dndOrderedColumns)
     }
+
+    setActiveDragItemId(null)
+    setActiveDragItemType(null)
+    setActiveDragItemData(null)
+  }
+
+  const dropAnimation = {
+    sideEffects: defaultDropAnimationSideEffects({
+      styles: {
+        active: {
+          // phần tử khi thả sẽ làm mờ
+          opacity: '0.5'
+        }
+      }
+    })
   }
 
   const { mode, systemMode } = useColorScheme()
   // Nếu mode = system thì lấy systemMode, ngược lại lấy mode
   const resolvedMode = mode === 'system' ? systemMode : mode
   return (
-    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+    <DndContext sensors={sensors} onDragStart={handleDragStart} onDragEnd={handleDragEnd} >
       <Box sx={{
         bgcolor: resolvedMode === 'dark' ? '#34495e' : '#1976d2',
         width: '100%',
@@ -63,6 +98,11 @@ function BoardContent({ board }) {
         p: '10px 0'
       }}>
         <ListColumns columns={ oderedColumns } />
+        <DragOverlay dropAnimation={dropAnimation}>
+          {(!activeDragItemType) && null}
+          {(activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) && <Column column={activeDragItemData} />}
+          {(activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) && <Card card={activeDragItemData} />}
+        </DragOverlay>
       </Box>
     </DndContext>
   )
