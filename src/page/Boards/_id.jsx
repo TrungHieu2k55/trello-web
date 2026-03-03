62566233de7c5ffb0a153d5a60748ec454603a72
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2026-03-03 23:28:21 +0700

    re

--- src/page/Boards/_id.jsx
+++ src/page/Boards/_id.jsx
@@ -3,7 +3,7 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
 import { fetchBoardDetailsAPI, createNewColumnAPI, createNewCardAPI } from '~/apis/index'
 import { useState, useEffect } from 'react'
 
@@ -11,53 +11,61 @@ import { useState, useEffect } from 'react'
 function Board() {
   const [board, setBoard] = useState(null)
 
-  // useEffect(() => {
-  //   const boardId = '699c1e1facd2d1f67ce15f4f'
-  //   //call api
-  //   fetchBoardDetailsAPI(boardId).then(board => {
-  //     setBoard(board)
-  //   })
-  // }, [])
+  useEffect(() => {
+    const boardId = '699c1e1facd2d1f67ce15f4f'
+    //call api
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
 
-
-  //Function Gọi API tạo mới column và làm lại dữ liệu State Board
+  // Func gọi API tạo mới Column làm lại dữ liệu state board
   const createNewColumn = async (newColumnData) => {
+
+    // Gọi API tạo column mới
     const createdColumn = await createNewColumnAPI({
       ...newColumnData,
-      boardId: board._id
+      boardId: board._id // hoặc boardId nếu tên biến là boardId
     })
 
     // Cập nhật state board
     const newBoard = { ...board }
-    newBoard.columns.push(createdColumn)
-    newBoard.columnOrderIds.push(createdColumn._id)
+    board.columns.push(createdColumn)
+    board.columnOrderIds.push(createdColumn._id)
+
+    // Cập nhật state
     setBoard(newBoard)
   }
 
-  //Function Gọi API tạo mới Card và làm lại dữ liệu State Board
+  // Func gọi API tạo mới Card làm lại dữ liệu state board
   const createNewCard = async (newCardData) => {
+    // Gọi API tạo column mới
     const createdCard = await createNewCardAPI({
       ...newCardData,
-      boardId: board._id
+      boardId: board._id // hoặc boardId nếu tên biến là boardId
     })
 
+    // Cập nhật state board
     const newBoard = { ...board }
     const columnToUpdate = newBoard.columns.find(column => column._id === createdCard.columnId)
     if (columnToUpdate) {
       columnToUpdate.cards.push(createdCard)
       columnToUpdate.cardOrderIds.push(createdCard._id)
     }
+
+
+    // Cập nhật state
     setBoard(newBoard)
   }
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData.board}/>
+      <BoardBar board={board}/>
       <BoardContent
-        board={mockData.board}
-        createNewColumn={createNewColumn}
+        board={board}
         createNewCard={createNewCard}
+        createNewColumn={createNewColumn}
       />
     </Container>
   )
