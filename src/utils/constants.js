4a197aafb04306f991cd36fb594b45fbe8d715e3
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2026-04-05 00:20:11 +0700

    Fix process env buildmode and install cross

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const API_ROOT = 'http://localhost:8889'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8889'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-x4te.onrender.com'
+}
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Cho phép vite xài process.env, bình thường xài meta.env
+  // https://github.com/vitejs/vite/issues/1973
+  define: {
+    'process.env' : process.env
+  },
   plugins: [
     react(),
     svgr()
