484b4b7b12bf32fbfc2c723b21086c610c3d9553
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2026-07-14 22:50:42 +0700

    redux persist & cookie

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 
 
 import Board from '~/page/Boards/_id'
 import NotFound from '~/page/404/NotFound'
 import Auth from '~/page/Auth/Auth'
 import AccountVerification from '~/page/Auth/AccountVerification'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '~/redux/user/userSlice'
+
+const PrivateRoute = ({ user }) => {
+  if (!user) return <Navigate to='/login' replace={true} />
+  return <Outlet />
+}
 
 function App() {
+  const currentUser = useSelector(selectCurrentUser)
   return (
     <Routes>
       <Route path='/' element={
         <Navigate to='/boards/699c1e1facd2d1f67ce15f4f' replace={true} />
       } />
 
-      {/* Board Details */}
-      <Route path='/boards/:boardId' element={<Board />} />
+      <Route element={<PrivateRoute user={currentUser} />}>
+        {/* Board Details */}
+        <Route path='/boards/:boardId' element={<Board />} />
+      </Route>
 
       {/* Authencation */}
       <Route path='/login' element={<Auth/>} />
--- src/main.jsx
+++ src/main.jsx
@@ -14,24 +14,31 @@ import { Provider } from 'react-redux'
 import { store } from '~/redux/store'
 
 // Cấu hình react-router-dom với BrowserRouter
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+//Cấu hình Redux-persist
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/'>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <ConfirmProvider defaultOptions={{
-          allowClose: false,
-          dialogProps: { maxWidth : 'xs' },
-          cancellationButtonProps: { color: 'inherit' },
-          confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
-          buttonOrder: ['confirm', 'cancel']
-        }}>
-          <CssBaseline />
-          <App />
-          <ToastContainer theme='colored' />
-        </ConfirmProvider>
-      </ThemeProvider>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <ConfirmProvider defaultOptions={{
+            allowClose: false,
+            dialogProps: { maxWidth : 'xs' },
+            cancellationButtonProps: { color: 'inherit' },
+            confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
+            buttonOrder: ['confirm', 'cancel']
+          }}>
+            <CssBaseline />
+            <App />
+            <ToastContainer theme='colored' />
+          </ConfirmProvider>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
